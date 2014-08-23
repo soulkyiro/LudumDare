9ba0af25486c8453624638615fe3dcab965a1f83
Silverio Cardona Rodríguez <silver.correo@gmail.com>
2014-08-23 16:43:21 +0100

    Added Signal put

---
+++ Assets/Scripts/Controller/SignalController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SignalController : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D other){
+		if (other.gameObject.tag == "Astronaut"){
+			Debug.Log ("Lo tienes");
+		}
+	}
+}
