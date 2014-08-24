7366713588c4bcd8f567324e7f27881cd9e6b17a
Estanislao Castro Nieblas <soulkyiro@gmail.com>
2014-08-24 14:46:54 +0100

    Added Sounds and more changes

--- Assets/Scripts/Controller/SoundController.cs
+++ Assets/Scripts/Controller/SoundController.cs
@@ -10,6 +10,26 @@ public class SoundController : MonoBehaviour
     private float tempVolumeEfect;
     private float tempVolumeMusic;
 
+    //Sounds
+    public  AudioSource rocketBag;
+    public AudioSource checkPointSignal;
+    public AudioSource music;
+
+    public AudioClip rocketBagClip = Resources.Load("Sounds/147796__joshun__rocket-take-off-sound") as AudioClip;
+    public AudioClip checkPointSignalClip = Resources.Load("Sounds/220173__gameaudio__spacey-1up-power-up") as AudioClip;
+    
+    //Music
+    public AudioClip musicClip = Resources.Load("Sounds/ONO_-_09_-_BETRAY_09") as AudioClip;
+
+    void Awake() {
+        //rocketBag = gameObject.AddComponent<AudioSource>();
+        //rocketBag.audio.clip = rocketBagClip;
+        //checkPointSignal = gameObject.AddComponent<AudioSource>();
+        //checkPointSignal.audio.clip = checkPointSignalClip;
+        music = gameObject.AddComponent<AudioSource>();
+        music.loop = true;
+        music.audio.clip = musicClip;
+    }
 	void Start () {
         sound = true;
         volumeMusic = 50.0F;
