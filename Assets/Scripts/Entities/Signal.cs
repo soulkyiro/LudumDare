4e3cd6a2e05441495f75e0a58adacfcf02d6a7c3
Silverio Cardona Rodríguez <silver.correo@gmail.com>
2014-08-24 16:22:41 +0100

    Fireworks!!

--- Assets/Scripts/Entities/Signal.cs
+++ Assets/Scripts/Entities/Signal.cs
@@ -3,17 +3,30 @@ using System.Collections;
 
 public class Signal: MonoBehaviour {
 
+	private bool activated = false;
+	private float fireworksLifetime = 5.0f;
+	public GameObject fireworksPrefab;
+
 	void OnTriggerEnter2D (Collider2D other){
 		if (other.gameObject.tag == "Astronaut"){
 				SetActive();
-				ApplicationController.Instance.GameController.LevelController.AddLetter(transform.parent.gameObject.name);
 		}
 	}
 
 	private void SetActive ()
 	{
-		gameObject.GetComponent<SpriteRenderer>().color = Color.green;
-        gameObject.GetComponent<AudioSource>().audio.Play();
-        Debug.Log(gameObject.GetComponent<AudioSource>().audio.isPlaying);
+		if (!activated){
+			ThrowFireworks();
+			gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+			gameObject.GetComponent<AudioSource>().audio.Play();
+			ApplicationController.Instance.GameController.LevelController.AddLetter(transform.parent.gameObject.name);
+			activated = true;
+		}
+	}
+
+	void ThrowFireworks ()
+	{
+		GameObject fireworks = GameObject.Instantiate (fireworksPrefab, transform.position, Quaternion.identity) as GameObject;
+		Destroy (fireworks, fireworksLifetime);
 	}
 }
